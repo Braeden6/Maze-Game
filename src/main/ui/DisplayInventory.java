a51479f8c9ee163d5bcc697a267b5cbd9d2f7deb
braedenn <braeden.norman6@gmail.com>
2020-04-16 18:31:38 -0700

    change inventory display to create its own window. They display itself needs to be updated still.

--- src/main/ui/DisplayInventory.java
+++ src/main/ui/DisplayInventory.java
@@ -9,18 +9,16 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
-public class DisplayInventory extends JPanel {
+public class DisplayInventory extends JFrame {
 
-    private GameConsoleInterface mainInterface;
+    private JPanel inventoryPanel;
     private Character mainCharacter;
 
     public DisplayInventory(GameConsoleInterface g, Character c) {
-        mainInterface = g;
+        super("Inventory");
         mainCharacter = c;
-        int w = GameMap.SCREEN_SIZE_WIDTH / 4;
-        int h = 30;
-        setPreferredSize(new Dimension(w,h));
-        setBackground(Color.white);
+        addPanel();
+        setSize(500,500);
     }
 
     //setter
@@ -28,14 +26,39 @@ public class DisplayInventory extends JPanel {
         mainCharacter = c;
     }
 
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        drawInventory(g);
+    // MODIFIES: mainInterface
+    // EFFECTS: enables display of inventory on main interface
+    public void enableDisplay() {
+        setVisible(true);
     }
 
+    // MODIFIES: mainInterface
+    // EFFECTS: disables display of inventory on main interface
+    public void disableDisplay() {
+        setVisible(false);
+    }
+
+
     // MODIFIES: this
-    // EFFECTS: display everything in mainCharacter inventory
+    // EFFECTS: initiates panel to for graphics and adds it to the inventory window
+    private void addPanel() {
+        int w = GameMap.SCREEN_SIZE_WIDTH / 4;
+        int h = 30;
+        inventoryPanel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                drawInventory(g);
+            }
+        };
+        inventoryPanel.setPreferredSize(new Dimension(w,h));
+        inventoryPanel.setBackground(Color.white);
+        add(inventoryPanel);
+    }
+
+
+    // MODIFIES: g
+    // EFFECTS: draws inventory to given graphics
     private void drawInventory(Graphics g) {
         int x = 0;
         g.setColor(Color.black);
@@ -44,21 +67,8 @@ public class DisplayInventory extends JPanel {
             g.drawString(s + "|", x, 20);
             x += s.length() * 10;
         }
-
     }
-
-    // MODIFIES: mainInterface
-    // EFFECTS: enables display of inventory on main interface
-    public void enableDisplay() {
-        mainInterface.add(this, BorderLayout.NORTH);
-    }
-
-    // MODIFIES: mainInterface
-    // EFFECTS: disables display of inventory on main interface
-    public void disableDisplay() {
-        mainInterface.remove(this);
-    }
-
+    
     //EFFECTS: displays key's names in given list. If they are the ground then the location will be displayed also.
     public ArrayList<String> getKeysString(LinkedList<Item> listOfKeys) {
         ArrayList<String> inventory = new ArrayList<>();
